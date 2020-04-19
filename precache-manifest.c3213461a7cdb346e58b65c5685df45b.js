6c08b46e3d0c7d5af6851d9390afa29e73203d14
Eddie Woodley <ecwoodley@gmail.com>
2020-04-19 21:38:54 +0100

    Updates

---
+++ precache-manifest.c3213461a7cdb346e58b65c5685df45b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb75f42a86b6e01fe7457f061a9a6cf1",
+    "url": "/trade-goods-calc/index.html"
+  },
+  {
+    "revision": "1380482645eb20a9dcbe",
+    "url": "/trade-goods-calc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b56b435cf7a2da205725",
+    "url": "/trade-goods-calc/static/js/2.98ac31a3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/trade-goods-calc/static/js/2.98ac31a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1380482645eb20a9dcbe",
+    "url": "/trade-goods-calc/static/js/main.4867850f.chunk.js"
+  },
+  {
+    "revision": "cc5e061daa7ad85f8453",
+    "url": "/trade-goods-calc/static/js/runtime-main.da074934.js"
+  }
+]);
